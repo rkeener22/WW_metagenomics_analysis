c910f5751b9c4db38e06ae81a04565363b1490d5
rkeener22 <rkeener.5@gmail.com>
2026-07-29 14:13:02 -0400

    Update megahit_multisample.sh

--- megahit_multisample.sh
+++ megahit_multisample.sh
@@ -2,7 +2,7 @@ for sample in /dodo/rk167/metafastp/norRNA/*_dedup_noribo.fastq.gz;
 
 do
 
-f=$(echo $sample | sed -E "s/\_.*//");
+s=$(echo $sample | sed -E "s/\_.*//");
 
-megahit -1 "$f"_1.fastq -2 "$f"_2.fastq -o "$file"_assembled.fasta/; 
+megahit -r "$s"_dedup_noribo.fastq.gz -o "$sample"_assembled.fasta/; 
 done
